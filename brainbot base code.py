2a111c80dd222be1e809375f71166732f969a0eb
LeoliyX <35179305+LeoliyX@users.noreply.github.com>
2020-01-24 11:14:47 -0800

    Add files via upload

---
+++ brainbot base code.py
@@ -0,0 +1,19 @@
+import discord
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('NjMzMDE1NzcyNjYyMjAyMzg4.XipJIQ.dWU0u7c22KduH1H2nRDifN6DgIo')
